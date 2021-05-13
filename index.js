dc21169600a60d8e2cb708213d5b40b792de257a
Pierce Novoa <piercenh17@gmail.com>
2021-05-13 17:07:12 -0500

    add create server with express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const http = require('http')
+const { request } = require('express')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
         id: 1,
-        "content": "Me gusta dibujar en blanco",
+        "content": "Me gusta dibujar en blanco y negro",
         "date": "2020-05-30T17:30:31.098Z",
         "important": true
     },
@@ -21,11 +23,33 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log({id})
+    const note = notes.find(note => note.id == id)
+    
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id != id)
+    response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`) 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`) 
+})
