5adfbf28cdb25a4c7a77cec6c1c00a7f34a08993
Pierce Novoa <piercenh17@gmail.com>
2021-05-13 21:50:55 -0500

    add post with REST Client

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { request } = require('express')
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -47,6 +49,31 @@ app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id != id)
     response.status(204).end()
+})
+
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if(!note || !note.content) {
+        return response.status(400).json({
+            error: 'note.content is missing'
+        })
+    }
+    
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+    
+    const newNote = {
+        id: maxId + 1,
+        content: note.content,
+        important: typeof note.important != 'undefined' ? note.important : false,
+        date: new Date().toISOString()
+    }
+
+    notes = notes.concat(newNote)
+
+    response.status(201).json(newNote)
 })
 
 const PORT = 3001
