dc76d5d8838d3af32f424f5e1018fc9721426c82
Pierce Novoa <piercenh17@gmail.com>
2021-05-13 23:22:54 -0500

    add installation and configuration eslint

--- index.js
+++ index.js
@@ -1,82 +1,81 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 let notes = [
-    {
-        id: 1,
-        "content": "Me gusta dibujar en blanco y negro",
-        "date": "2020-05-30T17:30:31.098Z",
-        "important": true
-    },
-    {
-        id: 2,
-        "content": "Tengo que estudiar MongoDB y NodeJS",
-        "date": "2021-06-30T18:39:34.091Z",
-        "important": false
-    },
-    {
-        id: 3,
-        "content": "Practicar piano 4 veces por semana",
-        "date": "2021-08-30T19:20:14.298Z",
-        "important": true
-    }
+  {
+    id: 1,
+    content: 'Me gusta dibujar en blanco y negro',
+    date: '2020-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Tengo que estudiar MongoDB y NodeJS',
+    date: '2021-06-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'Practicar piano 4 veces por semana',
+    date: '2021-08-30T19:20:14.298Z',
+    important: true
+  }
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+  response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log({id})
-    const note = notes.find(note => note.id == id)
-    
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+  const id = Number(request.params.id)
+  console.log({ id })
+  const note = notes.find((note) => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id != id)
-    response.status(204).end()
+  const id = Number(request.params.id)
+  notes = notes.filter((note) => note.id !== id)
+  response.status(204).end()
 })
 
-
 app.post('/api/notes', (request, response) => {
-    const note = request.body
+  const note = request.body
 
-    if(!note || !note.content) {
-        return response.status(400).json({
-            error: 'note.content is missing'
-        })
-    }
-    
-    const ids = notes.map(note => note.id)
-    const maxId = Math.max(...ids)
-    
-    const newNote = {
-        id: maxId + 1,
-        content: note.content,
-        important: typeof note.important != 'undefined' ? note.important : false,
-        date: new Date().toISOString()
-    }
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
 
-    notes = notes.concat(newNote)
+  const ids = notes.map((note) => note.id)
+  const maxId = Math.max(...ids)
 
-    response.status(201).json(newNote)
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = notes.concat(newNote)
+
+  response.status(201).json(newNote)
 })
 
 const PORT = 3001
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`) 
+  console.log(`Server running on port ${PORT}`)
 })
